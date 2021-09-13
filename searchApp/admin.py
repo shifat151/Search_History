a7ece22032605df657f57f15d32eb58c17e1a28a
Md Lutfor Rahman <shifat151@gmail.com>
2021-09-13 23:28:11 +0600

    models modified

--- searchApp/admin.py
+++ searchApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from searchApp.models import search_result, Search_detail
+
 # Register your models here.
+admin.site.register(search_result)
+admin.site.register(Search_detail)
--- searchApp/models.py
+++ searchApp/models.py
@@ -5,16 +5,28 @@ from django.db import models
 from django.db.models.base import Model
 from account.models import User
 
-class Search_details(models.Model):
+class Search_detail(models.Model):
     keyword=models.CharField(max_length=200)
     schedule=models.DateTimeField(auto_now_add=True, auto_now=False)
     user=models.ForeignKey(User, on_delete=models.CASCADE)
 
+    @property
+    def get_username(self):
+        return self.username
+    def __str__(self):
+        return "{} - ({})".format(self.keyword,self.user.username)
 
-class search_results(models.Model):
+
+
+class search_result(models.Model):
+    title=models.CharField(max_length=100)
     descripton=models.CharField(max_length=200)
     link=models.URLField()
-    details=models.ManyToManyField(Search_details)
+    details=models.ManyToManyField(Search_detail)
+
+    def __str__(self):
+        return self.title
+
 
 
 
